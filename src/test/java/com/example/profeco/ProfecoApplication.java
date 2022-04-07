d5510916e8c9bca24afb88d64134212490eb246c
Rene Hernandez <rene.he24@gmail.com>
2022-04-07 15:16:02 -0700

    Error en la conexion con la BD

---
+++ src/test/java/com/example/profeco/ProfecoApplication.java
@@ -0,0 +1,13 @@
+package com.example.profeco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public  class ProfecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfecoApplication.class, args);
+	}
+
+}
